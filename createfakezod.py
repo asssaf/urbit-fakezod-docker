26296f71ce9f465046bb67506c708a063b9f13e0
assaf <asssaf@localhost>
2017-08-25 22:58:38 -0700

    increase timeouts in expect script for docker hub builds

--- createfakezod.py
+++ createfakezod.py
@@ -2,9 +2,9 @@ import pexpect, sys
 
 child = pexpect.spawn ('urbit -F -I zod -A arvo -c fakezod')
 child.logfile = sys.stdout
-child.expect("activated app home/dojo", timeout=120)
-child.expect("~zod:dojo>", timeout=120)
+child.expect("activated app home/dojo", timeout=300)
+child.expect("~zod:dojo>", timeout=300)
 child.sendline("(mul 249 367)\r")
-child.expect("91.383")
+child.expect("91.383", timeout=120)
 child.sendline("|exit\r")
-child.expect(pexpect.EOF)
+child.expect(pexpect.EOF, timeout=120)
