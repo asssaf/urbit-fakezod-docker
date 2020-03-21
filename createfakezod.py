c2d2495f874f27c5b9063f3b5a22aa7d48be7460
assaf <asssaf@localhost>
2020-03-21 16:14:54 -0700

    increase creation timeout

--- createfakezod.py
+++ createfakezod.py
@@ -2,7 +2,7 @@ import pexpect, sys
 
 child = pexpect.spawn ('urbit -F zod')
 child.logfile = sys.stdout
-child.expect("activated app home/dojo", timeout=600)
+child.expect("activated app home/dojo", timeout=1800)
 child.expect("~zod:dojo>", timeout=600)
 child.sendline("(mul 249 367)\r")
 child.expect("91.383", timeout=120)
