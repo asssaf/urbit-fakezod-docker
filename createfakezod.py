dd1ccc9c9a7a5eca4f09178538f2aa4a5a647cbe
assaf <asssaf@localhost>
2020-10-02 12:50:20 -0700

    update expected pattern

--- createfakezod.py
+++ createfakezod.py
@@ -2,7 +2,7 @@ import pexpect, sys
 
 child = pexpect.spawn ('urbit -F zod')
 child.logfile = sys.stdout
-child.expect("activated app home/dojo", timeout=3600)
+child.expect("play: done", timeout=3600)
 child.expect("~zod:dojo>", timeout=1800)
 child.sendline("(mul 249 367)\r")
 child.expect("91.383", timeout=300)
